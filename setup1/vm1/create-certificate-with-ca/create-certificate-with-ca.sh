08cc4076756283c78b7363d08b270bce6db9481b
Rajesh <rbadrana@gmail.com>
2021-12-11 12:09:14 +0000

    MSP for org1

--- setup1/vm1/create-certificate-with-ca/create-certificate-with-ca.sh
+++ setup1/vm1/create-certificate-with-ca/create-certificate-with-ca.sh
@@ -1,13 +1,14 @@
 createcertificatesForOrg1() {
   echo
-  echo "Enroll the CA admin"
+  echo "Enrolling the CA Admin"
   echo
   mkdir -p ../crypto-config/peerOrganizations/org1.example.com/
   export FABRIC_CA_CLIENT_HOME=${PWD}/../crypto-config/peerOrganizations/org1.example.com/
 
   fabric-ca-client enroll -u https://admin:adminpw@localhost:7054 --caname ca.org1.example.com --tls.certfiles ${PWD}/fabric-ca/org1/tls-cert.pem
 
-  echo 'NodeOUs:
+  #below is Node orgnization unit which we have to put inside each MSP such as we have four units: Client, Orderer, Peer and Admin unit
+  echo 'NodeOUs:  
   Enable: true
   ClientOUIdentifier:
     Certificate: cacerts/localhost-7054-ca-org1-example-com.pem
@@ -22,6 +23,7 @@ createcertificatesForOrg1() {
     Certificate: cacerts/localhost-7054-ca-org1-example-com.pem
     OrganizationalUnitIdentifier: orderer' >${PWD}/../crypto-config/peerOrganizations/org1.example.com/msp/config.yaml
 
+  #first we have to register every entity or unit and then Enroll using fabric-ca-client to create MSP
   echo
   echo "Register peer0"
   echo
@@ -33,10 +35,15 @@ createcertificatesForOrg1() {
   fabric-ca-client register --caname ca.org1.example.com --id.name peer1 --id.secret peer1pw --id.type peer --tls.certfiles ${PWD}/fabric-ca/org1/tls-cert.pem
 
   echo
-  echo "Register user"
+  echo "Register user1"
   echo
   fabric-ca-client register --caname ca.org1.example.com --id.name user1 --id.secret user1pw --id.type client --tls.certfiles ${PWD}/fabric-ca/org1/tls-cert.pem
 
+  echo
+  echo "Register user2"
+  echo
+  fabric-ca-client register --caname ca.org1.example.com --id.name user2 --id.secret user2pw --id.type client --tls.certfiles ${PWD}/fabric-ca/org1/tls-cert.pem
+  
   echo
   echo "Register the org admin"
   echo
@@ -55,6 +62,7 @@ createcertificatesForOrg1() {
 
   cp ${PWD}/../crypto-config/peerOrganizations/org1.example.com/msp/config.yaml ${PWD}/../crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/msp/config.yaml
 
+  #tls certifactes are also generated and required for secure communication between units such as peers of same or diffrent organization
   echo
   echo "## Generate the peer0-tls certificates"
   echo
@@ -100,11 +108,19 @@ createcertificatesForOrg1() {
   mkdir -p ../crypto-config/peerOrganizations/org1.example.com/users
   mkdir -p ../crypto-config/peerOrganizations/org1.example.com/users/User1@org1.example.com
 
+  #we dont need to create msp for users or admin
   echo
-  echo "## Generate the user msp"
+  echo "## Generate the user1 msp"
   echo
   fabric-ca-client enroll -u https://user1:user1pw@localhost:7054 --caname ca.org1.example.com -M ${PWD}/../crypto-config/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp --tls.certfiles ${PWD}/fabric-ca/org1/tls-cert.pem
 
+  mkdir -p ../crypto-config/peerOrganizations/org1.example.com/users/User2@org1.example.com
+  
+  echo
+  echo "## Generate the user2 msp"
+  echo
+  fabric-ca-client enroll -u https://user2:user2pw@localhost:7054 --caname ca.org1.example.com -M ${PWD}/../crypto-config/peerOrganizations/org1.example.com/users/User2@org1.example.com/msp --tls.certfiles ${PWD}/fabric-ca/org1/tls-cert.pem
+  
   mkdir -p ../crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com
 
   echo
